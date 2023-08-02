41427623c97dac7281144f8c74a2e6e5f2755194
Natalia Konovalova <nutus@yandex.ru>
2023-08-02 19:09:34 +0300

    PR15_login_users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -100,7 +100,7 @@ const login = (req, res, next) => {
       return res.status(200).send({ email });
     })
     .catch(() => {
-      next(UnauthorizedError('Ошибка авторизации'));
+      next(new UnauthorizedError('Ошибка авторизации'));
     });
 };
 
